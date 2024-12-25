f3b4f682c94f02b1f791d6b5c3e28704fcd3a874
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-12-25 10:04:45 +0100

    done problem 5

--- 03_linked_list/Problem5.c
+++ 03_linked_list/Problem5.c
@@ -17,6 +17,22 @@ void* deleteLast(struct listNode** head);
 void* delete(struct listNode** head, int idx);
 void* deleteLinkedList(struct listNode** head);
 
+int kthNodeFromEnd(struct listNode* head, int k){
+    struct listNode* cur = head;
+    for (int i = 2; i <= k; i++) {
+        if (cur== NULL) {
+            printf("k=%d is greater than the length of the list\n", k);
+            return -1; // error, k is greater than the length of the list
+        }
+        cur = cur->next;
+    } 
+    while (cur->next != NULL) {
+        head = head->next;
+        cur = cur->next;
+    }
+    return head->data;
+}
+
 int main(){
 	struct listNode *head=NULL;
 
@@ -29,6 +45,10 @@ int main(){
     head = insertAtGivenPosition(head, 42, 5);
 	printSLList(head); // [32, 2, 2, 5, 42, 3, 42]
 
+    printf("%d\n", kthNodeFromEnd(head, 1)); // 42
+    printf("%d\n", kthNodeFromEnd(head, 2)); // 3
+    printf("%d\n", kthNodeFromEnd(head, 300)); // error
+    printf("%d\n", kthNodeFromEnd(head, 7)); // 32
 
     deleteLinkedList(&head);
 	printSLList(head); // []
